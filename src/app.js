fb0a13be705238b354627bea7f02824692ad78e4
JJongsKim <totnwjd530@gmail.com>
2023-05-16 16:20:02 +0900

    setting: 예민한 정보 가림, db연결

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const PORT = 8000;
+
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOST,
+  user: process.env.RDS_USER,
+  password: process.env.RDS_PASSWORD,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+
+  console.log("Connected to database.");
+});
+
+connection.end();
 
 app.get("/", (req, res) => res.send("Hello! Internet Programming 2Team"));
 app.listen(PORT, () => console.log("Test app listening on port 8000!"));
